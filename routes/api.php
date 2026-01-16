3b9e5e20a98590cd5b6f2751b97fc1df9ef081e9
abolifa <abulifa@outlook.com>
2026-01-16 22:34:56 +0200

    session

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,3 @@
 use App\Http\Controllers\VidecomGatewayController;
 
 Route::post('/v1/videcom/command', [VidecomGatewayController::class, 'command']);
-Route::post('/v1/videcom/history', [VidecomGatewayController::class, 'history']);
-Route::post('/v1/videcom/reset', [VidecomGatewayController::class, 'reset']);
